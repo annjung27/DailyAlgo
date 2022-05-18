30b0a1ada9c0671c42aa303b286926cfd0cf5f54
Annie Jung <younjujung27@gmail.com>
2022-05-18 09:49:52 -0700

    add d8

---
+++ d8_bookIndex.js
@@ -0,0 +1,87 @@
+/* 
+Book Index
+Given an array of ints representing page numbers
+return a string with the page numbers formatted as page ranges when the nums
+span a consecutive range.
+*/
+
+const nums1 = [1, 13, 14, 15, 37, 38, 70];
+const expected1 = "1, 13-15, 37-38, 70";
+
+const nums2 = [5, 6, 7, 8, 9];
+const expected2 = "5-9";
+
+const nums3 = [1, 2, 3, 7, 9, 15, 16, 17];
+const expected3 = "1-3, 7, 9, 15-17";
+
+/**
+ * Turns the given arr of page numbers into a string of comma hyphenated
+ * page ranges.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Array<number>} nums Page numbers.
+ * @returns {string} The given page numbers as comma separated hyphenated
+ *    page ranges.
+ */
+function bookIndex(nums) {
+  let pageNumsStr = "";
+  const separater = "-";
+
+  for(let i=0; i < nums.length; i++){
+    if( nums[i] === nums[i+1]-1){
+      pageNumsStr += nums[i] + separater;
+    } 
+    else if(i === nums.length-1){
+      pageNumsStr += nums[i];
+    } else {
+      pageNumsStr += nums[i] + ", ";
+    } 
+  }
+  return pageNumsStr;
+}
+
+console.log(bookIndex(nums1));
+console.log(bookIndex(nums2));
+console.log(bookIndex(nums3));
+
+
+
+
+/*****************************************************************************/
+
+/**
+ * - Time: O(n) linear.
+ * - Space: O(n) linear.
+ */
+function bookIndex(pageNums = "") {
+  let formattedPageNums = "";
+  let pageRangeStartIdx = 0;
+
+  for (let i = 0; i < pageNums.length; i++) {
+    let currentPageNum = pageNums[i];
+    let nextPageNum = pageNums[i + 1];
+    let pageRangeStopIdx = i;
+
+    // page range is not sequential
+    if (currentPageNum + 1 !== nextPageNum) {
+      // single page, no range
+      if (pageRangeStartIdx === pageRangeStopIdx) {
+        formattedPageNums += currentPageNum;
+      }
+      // we have a page range
+      else {
+        formattedPageNums += `${pageNums[pageRangeStartIdx]}-${pageNums[pageRangeStopIdx]}`;
+      }
+      if (i !== pageNums.length - 1) {
+        formattedPageNums += ", ";
+      }
+
+      // Since we've added a page range
+      // need to get start idx set up for next page range
+      pageRangeStartIdx = i + 1;
+    }
+  }
+  return formattedPageNums;
+}
+
+module.exports = { bookIndex };
